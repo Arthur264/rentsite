46f861a4a5c14e6b1c599c27c019acb5a14f9510
Arthur264 <Artyr2643@gmail.com>
2018-03-13 20:52:16 +0200

    two database

--- profile/urls.py
+++ profile/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, FavoritesView
 
 app_name = "profile"
 urlpatterns = [
-    url(r'$', IndexView.as_view(), name="index")
+    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^favorites/$', FavoritesView.as_view(), name="favorites"),
 ]
--- profile/views.py
+++ profile/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
+    login_url = '/house/'
+    redirect_field_name = 'redirect_to'
     template_name = 'profile.html'
+
+
+class FavoritesView(LoginRequiredMixin, TemplateView):
+    login_url = '/house/'
+    redirect_field_name = 'redirect_to'
+    template_name = 'profilefavorites.html'
